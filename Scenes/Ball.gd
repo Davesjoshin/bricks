015a2a66d8299edf93cc447064c08f9d161a7001
David Walz <david@makedigitalgroup.com>
2019-12-28 12:34:14 -0600

    initial commit - basic mechanics working

---
+++ Scenes/Ball.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+var bricks_in_row = 0
+var point_multi = 1
+export var base_point = 5
+
+export var speedup = 1.025
+const MAXSPEED = 500
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	var bodies = get_colliding_bodies()
+	
+	for body in bodies:
+		# Kill bricks when hit
+		if body.is_in_group("bricks"):
+			bricks_in_row += point_multi
+			get_node("/root/World").score += bricks_in_row * base_point
+			
+			body.queue_free()
+		
+		# Change speed & direction when hitting paddle
+		if body.get_name() == "Paddle":
+			bricks_in_row = 0
+			var velocity = get_linear_velocity()*speedup
+			
+			if velocity.x >= MAXSPEED:
+				velocity.x = MAXSPEED
+				
+			if velocity.y >= MAXSPEED:
+				velocity.y = MAXSPEED
+				
+			set_linear_velocity(velocity)
+	
+	# Ball reaches bottom of screen
+	if get_position().y > get_viewport_rect().end.y:
+		queue_free()
+		print("ball is gone")
+		get_node("/root/World").score -= base_point
---
+++ Scripts/Paddle.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const ball_scene = preload("res://Mini Scenes/Ball.tscn")
+var motion = Vector2()
+var speed = 20
+
+func _ready():
+	set_physics_process(true)
+	set_process_input(true)
+
+func _physics_process(delta):
+	motion.y = get_position().y
+	# var mouse_x = get_viewport().get_mouse_position().x
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = motion.x + speed
+		if motion.x >= 925:
+			motion.x = 925
+			print(motion.x)
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = motion.x - speed
+		if motion.x <= 100:
+			motion.x = 100
+
+	else:
+		motion.x = get_position().x
+	
+	# set_position(Vector2(mouse_x, y))
+	set_position(motion)
+
+
+func _input(event):
+	if event is InputEventKey and event.scancode == KEY_UP and event.pressed == false:
+		var ball = ball_scene.instance()
+		ball.set_position(get_position()-Vector2(0,16))
+		get_tree().get_root().add_child(ball)
